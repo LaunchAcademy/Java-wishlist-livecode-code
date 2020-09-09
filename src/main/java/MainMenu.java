740a746e82d82328e6db12b263cbc24622b67bb1
DanAlberts <52653235+DanAlberts@users.noreply.github.com>
2020-09-09 18:01:49 -0400

    code

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 
 public class MainMenu {
+
   private static Scanner input = new Scanner(System.in);
   private static EntityManagerFactory emf;
   private static EntityManager em;
@@ -14,19 +16,19 @@ public class MainMenu {
   public static void main(String[] args) throws IOException {
     menu();
   }
-
   //Create a menu with options
   public static void menu(){
     emf = Persistence.createEntityManagerFactory("com.launchacademy.wishlist");
     em = emf.createEntityManager();
-    String[] options = {". Add a new product", ". View wishlist", ". Exit"};
+    String[] options = {". Add a new item", ". View wishlist", ". Exit"};
     while(true){
-      for (int i = 0; i < options.length; i++){
+      for (int i = 0 ; i < options.length; i++){
         System.out.println((i + 1) + options[i]);
       }
-      System.out.println("CHOOSE a number HUMAN");
+      System.out.println("CHOOSE A NUMBER HUMAN!");
       int choice = input.nextInt();
-      if( choice == options.length){
+      if (choice == options.length){
+        System.out.println("Goodbye");
         em.close();
         emf.close();
         System.exit(0);
@@ -40,28 +42,78 @@ public class MainMenu {
     }
   }
 
+
   //Add a product
-  private static void addProduct(){
+  public static void addProduct() {
+    String name = "";
+    String price = "";
+    String url = "";
+    String category = "";
+
+    System.out.println("Name of product");
+    Scanner userInputName = new Scanner(System.in);
+    name = userInputName.nextLine();
+
+    System.out.println("Price of product");
+    Scanner userInputPrice = new Scanner(System.in);
+    price = userInputPrice.nextLine();
+    Float floatPrice = Float.parseFloat(price);
+
+    System.out.println("Url to product");
+    Scanner userInputUrl = new Scanner(System.in);
+    url = userInputUrl.nextLine();
+
+    System.out.println("Would you like to add a category? (type 'none' for no");
+    Scanner inputCategory = new Scanner(System.in);
+    category = inputCategory.nextLine();
+
+    Product product = new Product();
+    Category newCategory = new Category();
+
+    TypedQuery<Category> categoryQuery = em.createQuery("SELECT c FROM Category c", Category.class);
+    List<Category> categories = categoryQuery.getResultList();
+
     try{
-      Product newProduct = new Product();
-      System.out.println("What product would you like to add?");
-      newProduct.setName(input.next());
-      System.out.println("What is the product's price?");
-      newProduct.setPrice(input.nextFloat());
-      System.out.println("What is the product's url?");
-      newProduct.setUrl(input.next());
+
+      newCategory.setName(category);
       em.getTransaction().begin();
-      em.persist(newProduct);
+      em.persist(newCategory);
       em.getTransaction().commit();
-    } finally {
+
+      em.getTransaction().begin();
+      product.setName(name);
+      product.setPrice(floatPrice);
+      product.setUrl(url);
+      product.setCategory(newCategory);
+
+      em.persist(product);
+      em.getTransaction().commit();
+
+    } catch (Exception ex){
+      System.out.println("Duplicate Category Name Error");
+      em.getTransaction().begin();
+      product.setName(name);
+      product.setPrice(floatPrice);
+      product.setUrl(url);
+      for (Category eachCategory : categories){
+        if(category.equals(eachCategory.getName())){
+          product.setCategory(eachCategory);
+        }
+      }
+      em.persist(product);
+      em.getTransaction().commit();
     }
   }
 
   //List all products in wishlist
   private static void listWishes(){
-    List<Product> wishes = em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
-    for (Product product : wishes){
-      System.out.println(product.getName() + " " + product.getPrice() + " " + product.getUrl());
+    TypedQuery<Product> query = em.createQuery("SELECT p FROM Product p", Product.class);
+    List<Product> products = query.getResultList();
+
+    for (Product product : products){
+      Category eachCategory = product.getCategory();
+      System.out.println("Product: " + product.getName() + "\n" + "Price: " + product.getPrice()
+      + "\n" + "Url: " + product.getUrl() + "\n" + " Category: " + eachCategory.getName() + "\n");
     }
   }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE products(
-              id SERIAL PRIMARY KEY,
-              name VARCHAR(255) NOT NULL,
-              price FLOAT,
-              url VARCHAR(255) NOT NULL
-              );
+CREATE table products(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  price FLOAT,
+  url VARCHAR(255) NOT NULL
+);
